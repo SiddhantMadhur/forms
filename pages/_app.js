08056f7282653a0844ac3ba653911b162ce250e5
SiddhantMadhur <siddhant.madhur@gmail.com>
2022-05-21 23:55:44 +0400

    project init

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/globals.css'
+
+
+function MyApp({Component, pageProps}) {
+  return (
+    <div>
+        <Component {...pageProps} />
+    </div>
+  )
+}
+
+export default MyApp
